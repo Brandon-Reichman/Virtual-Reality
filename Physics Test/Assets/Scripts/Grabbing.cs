784513d171c672e4acaf6e41aab52552dfc9639f
Brandon-Reichman <breich5@uic.edu>
2017-02-15 13:29:57 -0600

    Test Room for physics of balls/grab controls

---
+++ Physics Test/Assets/Scripts/Grabbing.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabbing : MonoBehaviour {
+
+
+    public OVRInput.Controller controller;
+    public string grabButton;
+    public float grabRadius;
+    public LayerMask grabMask;
+    private GameObject grabbedObject;
+    private bool grabbing;
+    private Quaternion lastRotation, currentRotation;
+    private Vector3 startScale;
+    public GameObject ball;
+
+    void Start()
+    {
+        startScale = ball.transform.localScale;
+    }
+    void GrabObject()
+    {
+        grabbing = true;
+        RaycastHit[] hits;
+        hits = Physics.SphereCastAll(transform.position, grabRadius, transform.forward, 0f, grabMask);
+
+        if (hits.Length > 0)
+        {
+            int closestHit = 0;
+            for (int i = 0; i < hits.Length; i++)
+            {
+                if (hits[i].distance > hits[closestHit].distance) closestHit = i;
+            }
+            
+            grabbedObject = hits[closestHit].transform.gameObject;
+            grabbedObject.transform.localScale = startScale;
+            grabbedObject.GetComponent<Rigidbody>().isKinematic = true;
+            grabbedObject.transform.parent = transform;
+            
+            
+
+        }
+    }
+
+    void DropObject()
+    {
+        grabbing = false;
+
+        if (grabbedObject != null)
+        {
+            grabbedObject.transform.parent = null;
+            grabbedObject.GetComponent<Rigidbody>().isKinematic = false;
+            grabbedObject.GetComponent<Rigidbody>().velocity = OVRInput.GetLocalControllerVelocity(controller);
+            grabbedObject.GetComponent<Rigidbody>().angularVelocity = GetAngularVelocity();
+            grabbedObject.transform.localScale = startScale;
+            grabbedObject = null;
+        }
+    }
+
+    Vector3 GetAngularVelocity()
+    {
+        Quaternion deltaRotation = currentRotation * Quaternion.Inverse(lastRotation);
+        return new Vector3(Mathf.DeltaAngle(0, deltaRotation.eulerAngles.x), Mathf.DeltaAngle(0, deltaRotation.eulerAngles.y), Mathf.DeltaAngle(0, deltaRotation.eulerAngles.z));
+    }
+
+
+
+	void Update ()
+    {
+        if (grabbedObject != null)
+        {
+            lastRotation = currentRotation;
+            currentRotation = grabbedObject.transform.rotation;
+        }
+
+        if (!grabbing && Input.GetAxis(grabButton) == 1)
+        {
+            Debug.Log("Grab Button Pressed");
+            GrabObject();
+        }
+        if (grabbing && Input.GetAxis(grabButton) < 1) DropObject();
+    }
+}
---
+++ Physics Test/Assets/Scripts/Reset.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour {
+
+    public GameObject ball;
+    private Vector3 startPosition;
+    private Quaternion startRotation;
+    private Vector3 startVelocity=new Vector3(0,0,0);
+    private Vector3 startSize;
+    public Rigidbody rigidbody;
+	// Use this for initialization
+	void Start () {
+        startPosition = ball.transform.position;
+        startRotation = ball.transform.rotation;
+        startVelocity = ball.transform.localEulerAngles;
+        startSize = ball.transform.localScale;
+        rigidbody = GetComponent<Rigidbody>();
+	}
+
+    void ResetBall()
+    {
+        ball.transform.position = startPosition;
+        ball.transform.rotation = startRotation;
+        ball.transform.localScale = startSize;
+        rigidbody.velocity = startVelocity;
+    }
+	// Update is called once per frame
+	void Update ()
+    {
+        if (OVRInput.Get(OVRInput.Button.One)) ResetBall();
+		
+	}
+}
